b6ecad558991fd4595a52a162c5b56e0a1edf123
leonardo silva <leonardohsilva.dev@gmail.com>
2020-04-20 22:52:05 -0300

    Criação menu lateral (drawer/side menu)

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'lista-agendamentos', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule) },
   {
     path: 'detalhes',
--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,6 +1,7 @@
+import { Router, RouterEvent } from '@angular/router';
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, MenuController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -10,12 +11,32 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  pages = [
+    {
+      title: 'Carros',
+      url: 'home',
+      icon: 'car-outline'
+    },
+    {
+      title: 'Agendamentos',
+      url: 'lista-agendamentos',
+      icon: 'document-outline'
+    }
+  ];
+
+  selectedPath = '';
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private menu: MenuController,
+    private router: Router
   ) {
     this.initializeApp();
+    this.router.events.subscribe((event: RouterEvent) => {
+      this.selectedPath = event.url;
+    })
   }
 
   initializeApp() {
@@ -24,4 +45,10 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
+  openMenu() {
+    this.menu.enable(true, 'menu');
+    this.menu.open('menu');
+  }
+
 }
